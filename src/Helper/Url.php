31b911e934764fe725a0cad23494b278d44d1fe7
Dima Nagorniy <dima.nagorniy@gmail.com>
2015-11-09 10:14:27 +0200

    Dima Nagorniy - Ulr.php added combine function and some help functions. Added tests for combine functions.

--- src/Helper/Url.php
+++ src/Helper/Url.php
@@ -43,7 +43,7 @@ class Url
    * Replaces in HTML code relative URLs with absolute URLs.
    *
    * @param string $theHtml The HTML code.
-   * @param string $theRoot The part of the URL's before the path part without slash.
+   * @param string $theRoot The part of the URLs before the path part without slash.
    *
    * @return string
    */
@@ -55,6 +55,109 @@ class Url
   }
 
   //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Combine URL and RelativeURL to single URL and return.
+   *
+   * @param string $theUri         The part of the URLs before the path part without slash.
+   * @param string $theRelativeUri The path part of the URLs.
+   *
+   * @return string
+   */
+  public static function combine($theUri, $theRelativeUri)
+  {
+    $Uri         = parse_url($theUri);
+    $RelativeUri = parse_url($theRelativeUri);
+
+    switch (true)
+    {
+      case !empty($RelativeUri['scheme']) || !empty($RelativeUri['host']):
+        break;
+      case empty($RelativeUri['path']):
+        $RelativeUri['path'] = self::normalize_path($Uri['path']);
+        if (empty($RelativeUri['query']))
+        {
+          $RelativeUri['query'] = $Uri['query'];
+        }
+        break;
+      case strpos($RelativeUri['path'], '/')===0:
+        break;
+      default:
+        $path = $Uri['path'];
+        if (strpos($path, '/')===false)
+        {
+          $path = '';
+        }
+        else
+        {
+          $path = preg_replace('/\/[^\/]+$/', '/', $path);
+        }
+        if (empty($path) && empty($Uri['host']))
+        {
+          $path = '/';
+        }
+        $RelativeUri['path'] = self::normalize_path($path.$RelativeUri['path']);
+    }
+    if (empty($RelativeUri['scheme']))
+    {
+      $RelativeUri['scheme'] = $Uri['scheme'];
+      if (empty($RelativeUri['host']))
+      {
+        $RelativeUri['host'] = $Uri['host'];
+      }
+    }
+    if(empty($RelativeUri['user'])){$RelativeUri['user'] = !empty($Uri['user']) ? $Uri['user'] : '';}
+    if(empty($RelativeUri['pass'])){$RelativeUri['pass'] = !empty($Uri['pass']) ? $Uri['pass'] : '';}
+    if(empty($RelativeUri['port'])){$RelativeUri['port'] = !empty($Uri['port']) ? $Uri['port'] : '';}
+
+    return self::unparse_url($RelativeUri);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Get parsed_url from parse_url('Url') and return full Url
+   *
+   * @param Array $parsed_url
+   *
+   * @return string
+   */
+  public static function unparse_url($parsed_url)
+  {
+    $scheme   = !empty($parsed_url['scheme']) ? "{$parsed_url['scheme']}://" : '';
+    $host     = !empty($parsed_url['host']) ? $parsed_url['host'] : '';
+    $port     = !empty($parsed_url['port']) ? ":{$parsed_url['port']}" : '';
+    $user     = !empty($parsed_url['user']) ? $parsed_url['user'] : '';
+    $pass     = !empty($parsed_url['pass']) ? ":{$parsed_url['pass']}" : '';
+    $pass     = ($user || $pass) ? "{$pass}@" : '';
+    $path     = !empty($parsed_url['path']) ? $parsed_url['path'] : '';
+    $query    = !empty($parsed_url['query']) ? "?{$parsed_url['query']}" : '';
+    $fragment = !empty($parsed_url['fragment']) ? "#{$parsed_url['fragment']}" : '';
+
+    return "$scheme$user$pass$host$port$path$query$fragment";
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Normalize path to format with slashes only
+   *
+   * @param string $path
+   *
+   * @return string
+   */
+  public static function normalize_path($path)
+  {
+    if (empty($path))
+    {
+      return '';
+    }
+    $normalized_path = $path;
+    $normalized_path = preg_replace('`//+`', '/', $normalized_path, -1, $c0);
+    $normalized_path = preg_replace('`^/\\.\\.?/`', '/', $normalized_path, -1, $c1);
+    $normalized_path = preg_replace('`/\\.(/|$)`', '/', $normalized_path, -1, $c2);
+    $normalized_path = preg_replace('`/[^/]*?/\\.\\.(/|$)`', '/', $normalized_path, 1, $c3);
+    $num_matches     = $c0 + $c1 + $c2 + $c3;
+
+    return ($num_matches>0) ? self::normalize_path($normalized_path) : $normalized_path;
+  }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
--- test/Helper/UrlTest.php
+++ test/Helper/UrlTest.php
@@ -78,6 +78,57 @@ class UrlTest extends PHPUnit_Framework_TestCase
   }
 
   //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test for combine.
+   */
+  public function testcombine()
+  {
+    $cases = [];
+
+    $cases[] = ['relative' => 'http://a1:a2@a:8080/b/c/g', 'result' => 'http://a1:a2@a:8080/b/c/g'];
+    $cases[] = ['relative' => 'g', 'result' => 'http://a/b/c/g'];
+    $cases[] = ['relative' => './g', 'result' => 'http://a/b/c/g'];
+    $cases[] = ['relative' => 'g/', 'result' => 'http://a/b/c/g/'];
+    $cases[] = ['relative' => '/g', 'result' => 'http://a/g'];
+    $cases[] = ['relative' => '//g', 'result' => 'http://g'];
+    $cases[] = ['relative' => 'g?y', 'result' => 'http://a/b/c/g?y'];
+    $cases[] = ['relative' => '#s', 'result' => 'http://a/b/c/d;p?q#s'];
+    $cases[] = ['relative' => 'g#s', 'result' => 'http://a/b/c/g#s'];
+    $cases[] = ['relative' => 'g?y#s', 'result' => 'http://a/b/c/g?y#s'];
+    $cases[] = ['relative' => ';x', 'result' => 'http://a/b/c/;x'];
+    $cases[] = ['relative' => 'g;x', 'result' => 'http://a/b/c/g;x'];
+    $cases[] = ['relative' => 'g;x?y#s', 'result' => 'http://a/b/c/g;x?y#s'];
+    $cases[] = ['relative' => '.', 'result' => 'http://a/b/c/'];
+    $cases[] = ['relative' => './', 'result' => 'http://a/b/c/'];
+    $cases[] = ['relative' => '..', 'result' => 'http://a/b/'];
+    $cases[] = ['relative' => '../', 'result' => 'http://a/b/'];
+    $cases[] = ['relative' => '../g', 'result' => 'http://a/b/g'];
+    $cases[] = ['relative' => '../..', 'result' => 'http://a/'];
+    $cases[] = ['relative' => '../../', 'result' => 'http://a/'];
+    $cases[] = ['relative' => '../../g', 'result' => 'http://a/g'];
+    $cases[] = ['relative' => 'g.', 'result' => 'http://a/b/c/g.'];
+    $cases[] = ['relative' => '.g', 'result' => 'http://a/b/c/.g'];
+    $cases[] = ['relative' => 'g..', 'result' => 'http://a/b/c/g..'];
+    $cases[] = ['relative' => '..g', 'result' => 'http://a/b/c/..g'];
+    $cases[] = ['relative' => './../g', 'result' => 'http://a/b/g'];
+    $cases[] = ['relative' => './g/.', 'result' => 'http://a/b/c/g/'];
+    $cases[] = ['relative' => 'g/./h', 'result' => 'http://a/b/c/g/h'];
+    $cases[] = ['relative' => 'g/../h', 'result' => 'http://a/b/c/h'];
+    $cases[] = ['relative' => 'g;x=1/./y', 'result' => 'http://a/b/c/g;x=1/y'];
+    $cases[] = ['relative' => 'g;x=1/../y', 'result' => 'http://a/b/c/y'];
+    $cases[] = ['relative' => 'g?y/./x', 'result' => 'http://a/b/c/g?y/./x'];
+    $cases[] = ['relative' => 'g?y/../x', 'result' => 'http://a/b/c/g?y/../x'];
+    $cases[] = ['relative' => 'g#s/./x', 'result' => 'http://a/b/c/g#s/./x'];
+    $cases[] = ['relative' => 'g#s/../x', 'result' => 'http://a/b/c/g#s/../x'];
+
+    foreach ($cases as $case)
+    {
+      $url = Url::combine('http://a/b/c/d;p?q', $case['relative']);
+      $this->assertEquals($case['result'], $url);
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
 }
 
 //----------------------------------------------------------------------------------------------------------------------
